dae16941c80097473e1a8426dea5e1eb999b909d
Hyrum Dickinson <hyrumsd2@illinois.edu>
2021-06-16 16:52:49 -0500

    Revert "Delete Setup.py"
    This reverts commit 023aacb0a057b8651ef8fe312d525f7176f14422.

---
+++ Setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+APP = ['SolarPanelMonitor.py']
+DATA_FILES = ['sync.png']
+OPTIONS = {
+ 'iconfile':'logoapp.icns',
+ 'argv_emulation': True,
+ 'packages': ['certifi'],
+}
+
+setup(
+    app=APP,
+    data_files=DATA_FILES,
+    options={'py3app': OPTIONS},
+    setup_requires=['py3app'],
+)
