7852a9d21ee90c36801b1da193e05d4e71593270
Hyrum Dickinson <hyrumsd2@illinois.edu>
2021-06-26 17:14:53 +0200

    added comments explaining the function of each color
    I'm thinking of changing the local color variable names from their corresponding colors to their corresponding functions. Whichever one I use for a name, the other should be a comment to the left to explain the name

--- Settings.py
+++ Settings.py
@@ -13,11 +13,11 @@ MIN_VOLTAGE_TRIP_POINT = 0.0
 MIN_CURRENT_TRIP_POINT = 0.0
 MIN_TEMPERATURE_TRIP_POINT = -40.0
 
-WHITE = '#ffffff'
-LIGHT_GRAY = '#ababab'
-MID_GRAY_1 = '#464646'
-MID_GRAY_2 = '#383735'
+WHITE = '#ffffff' # this color highlights buttons when they are not being pressed
+LIGHT_GRAY = '#ababab' # this color highlights the data widget and should not be changed
+MID_GRAY_1 = '#464646' # this color highlights the connection widget
+MID_GRAY_2 = '#383735' # this color highlights the space between and around the main widgets
 DARK_GRAY = '#262523'
-BLACK = '#000000'
+BLACK = '#000000' # this color highlights text
 RED = '#ff6262'
-GREEN = '#32cd32'
+GREEN = WHITE  # '#32cd32' # this color highlights interactive components when they are interacted with
