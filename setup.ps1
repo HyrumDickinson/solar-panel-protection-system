19d7237172cc617c06dbcd7a36cb39d7f7a12abb
Hyrum Dickinson <hyrumsd2@illinois.edu>
2021-07-23 14:56:41 -0500

    script to set up application on new computer

--- setup.ps1
+++ setup.ps1
@@ -5,16 +5,16 @@
 # You can run the commands verbatim, one line at a time, or you can make a copy of this file on your computer and then tell your computer to run it in Anaconda Powershell Prompt.
 
 git clone https://github.com/HyrumDickinson/Solar_Panel_Protection_System
-# if you are not an co-owner of this repository, do not run the line above. 
-# Instead, use your account at github.com to fork the URL, and then git clone the new URL created by your fork
+# If you are not an co-owner of this repository, do not run the line above. 
+# Instead, use your account at github.com to fork the URL, and then git clone the new URL created by your fork.
 conda create --name python2_solarpanels python=2
 conda activate python2_solarpanels
 conda install pillow
-# type Y when prompted
+# When prompted, type "Y".
 conda install matplotlib
-# type Y when prompted
+# When prompted, type "Y".
 python Main.py
-# after running this command, the application should load onto your screen. 
+# After you run this command, the application should load onto your screen. 
 
 # Disclaimer: This script has only been tested in Windows 10.
-# Disclaimer: Tf you do not have a compatible hardware system setup to run in tandem with the GUI, this application will lack most of its functionality.
+# Disclaimer: Tf you do not have a compatible hardware system setup to run in tandem with the application, it will lack most of its functionality.
