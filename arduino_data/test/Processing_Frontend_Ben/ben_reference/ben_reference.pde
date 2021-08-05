10f9dfa931f76280a9e21b2afa17b63d9c9aa075
ben_olaivar <olaivar3@illinois.edu>
2021-08-05 17:19:49 -0500

    added method to store node data in an array

--- arduino_data/test/Processing_Frontend_Ben/ben_reference/ben_reference.pde
+++ arduino_data/test/Processing_Frontend_Ben/ben_reference/ben_reference.pde
@@ -21,12 +21,50 @@ int newLine = 10;        // 10 is binary for 'return' or 'new line'.
 
 
 void setup() {
-  mySerial = new Serial(this, "COM4", 9600);
-  size(1200, 600);  // siza deez app windows (x and y dimensions of application window)
+  mySerial = new Serial(this, "COM6", 9600);
+  /* Creating array of data objects to record each node's data */
+  data[] dataArray = new data[60];
+  for (int i = 0; i < 60; i++) {
+    dataArray[i] = new data();
+  }
+  
+  size(1500, 800);  // siza deez app windows (x and y dimensions of application window)
   font = loadFont("SansSerif.plain-16.vlw");
   textFont(font);
 
   cp5 = new ControlP5(this);
+  PFont p = createFont("Verdana",16); 
+  ControlFont controlFont = new ControlFont(p);
+  cp5.setFont(controlFont);
+  
+  cp5.getTab("default")
+     .activateEvent(true)
+     .setLabel("Overview")
+     .setId(1)
+     .setHeight(40)
+     .setWidth(100)
+     ;
+  cp5.addTab("Nodes")
+    .setHeight(40)
+    .setWidth(100)
+    ;
+  
+  DropdownList droplist = cp5.addDropdownList("Nodes")
+    .setPosition(0, 40).setSize(90, 700)
+    .setBarHeight(40)
+    .setItemHeight(50)
+    .setBackgroundColor(color(213, 216, 220))
+    .align(0, 100, 100, 0);
+  
+  for (int i = 0; i < 60; i++) {
+    droplist.addItem("Node: " + i,i);
+  }
+  
+  
+  
+  
+  
+  
   cp5.addButton("node1")    //"Node 1" is the name of the button
     .setPosition(100, 50)
     .setSize(100, 50);
---
+++ arduino_data/test/Processing_Frontend_Ben/ben_reference/data/dataStorage.java
@@ -0,0 +1,166 @@
+class data {
+  /* OBJECT VARIABLES */
+  float temp1;
+  float temp2;
+  float temp3;
+  float voltage;
+  float current;
+  boolean overHeat;
+  boolean overVolt;
+  boolean overCurr;
+  boolean shutdown;
+
+  /* GENERAL VARIABLES */
+  float tempThreshFarenheit = 10;       //! arbitrary number chosen for testing
+  float voltageTrhesh = 10;             //! arbitrary number chosen for testing
+  float currentThresh = 10;             //! arbitrary number chosen for testing
+
+  /* Default constructor
+  *     Sets all object variables to 0/false
+  */
+  public data() {
+  	temp1 = 0;
+    temp2 = 0;
+    temp3 = 0;
+    voltage = 0;
+    current = 0;
+    overHeat = false;
+    overVolt = false;
+    overCurr = false;
+    shutdown = false;
+  }
+  
+  /* Constructor
+  *     Sets all object variables to given values
+  */
+  public data(
+      float t1, 
+      float t2, 
+      float t3, 
+      float v, 
+      float c, 
+      boolean oh, 
+      boolean ov, 
+      boolean oc,
+      boolean sd
+    ) {
+  	temp1 = t1;
+    temp2 = t2;
+    temp3 = t3;
+    voltage = v;
+    current = c;
+    overHeat = oh;
+    overVolt = ov;
+    overCurr = oc;
+    shutdown = sd;
+  }  
+  
+  public static void main(String[] args) {
+  	data[] dataArray = new data[60];
+    
+    for (int i = 0; i < 60; i++) {
+    	dataArray[i] = new data();
+    }
+    
+    
+    for (int i = 0; i < 60; i++) {
+        System.out.println(i + ":");
+        dataArray[i].setTemps(68, 59, 105);
+        float[] tempsF = dataArray[i].getTempsFarenheit();
+        System.out.println("Farenheit: "+ tempsF[0] + ", " + tempsF[1] + ", " + tempsF[2]);
+        System.out.println(dataArray[i].isShutdown());
+    	System.out.println("---------------------------");
+    }
+  }
+
+  /* Function: setTemps
+  *     Sets temperature in degrees Farenheit (data from each node is in Farenheit)
+  *     groups all 3 temperature values together to update temps (can be easily modified if we add more/remove temp sensors)
+  */
+  void setTemps(float t1, float t2, float t3) {
+  	temp1 = t1;
+    temp2 = t2;
+    temp3 = t3;
+    
+    if (t1 > tempThreshFarenheit) {
+        overHeat = true;
+        this.setShutdown(true);
+    } else {
+        overHeat = false;
+        this.setShutdown(false);
+    }
+  }
+  /* Function: getTempsFarenheit
+  *     return the temperature values (FARENHEIT) in a float array. This can easily be modified if we add/remove temp sensors
+  */
+  float[] getTempsFarenheit() {
+    float[] tempArray = {temp1, temp2, temp3};
+    return tempArray;
+  }
+  /* Function: getTempsCelcius
+  *     return the temperature values (CELCIUS) in a float array. This can easily be modified if we add/remove temp sensors
+  */
+  float[] getTempsCelcius() {
+    float[] temps = getTempsFarenheit();
+    for (int i = 0; i < temps.length; i++) {    // left as temps.length just in case we add/remove temp sensors
+        temps[i] -= 32;
+        temps[i] *= 5;
+        temps[i] /= 9;
+    }
+    return temps;
+  }
+
+  /* Function: setVoltage
+  *     sets the voltage in Volts
+  */
+  void setVoltage(float v) {
+  	voltage = v;
+    if (v > voltageTrhesh) {
+    	overVolt = true;
+        this.setShutdown(true);
+    } else {
+    	overVolt = false;
+        this.setShutdown(false);
+    }
+  }
+  /* Function: getVoltage
+  *     returns the voltage in Volts
+  */
+  float getVoltage() {
+    return voltage;
+  }
+
+  /* Function: setCurrent
+  *     sets teh current in Amps
+  */
+  void setCurrent(float c) {
+  	current = c;
+    if (c > currentThresh) {
+    	overCurr = true;
+        this.setShutdown(true);
+    } else {
+    	overCurr = false;
+        this.setShutdown(false);
+    }
+  }
+  /* Function: getCurrent
+  *     returns the current in Amps
+  */
+  float getCurrent() {
+    return current;
+  }
+  
+  /* Function: setShutdown
+  *     changes the state of the 'shutdown' boolean. Other methods call this function
+  */
+  void setShutdown(boolean s) {
+  	shutdown = s;
+  }
+  /* Function: getCurrent
+  *     returns true if node is in shutdown mode, else returns false
+  */
+  boolean isShutdown() {
+  	return shutdown;
+  } 
+
+}
